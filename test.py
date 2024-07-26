8a1b32c3e3fc3f8bd185c3a5feb533c7dbf694fe
yudai <yudai.yy0804@gmail.com>
2024-07-26 12:29:33 +0900

    合っているか怪しいけど、変調できてるっぽい

--- test.py
+++ test.py
@@ -1,9 +1,13 @@
 import numpy as np
+from scipy import interpolate
+import matplotlib.pyplot as plt
+import random
 
 # TODO: Nの数はあとでちゃんと考える
 
 # IFFT/FFTの回転因子の数
-N = 2048
+# N = 256
+N = 256
 # OFDMの周波数帯域は1~6kHz
 # OFDMの下限
 SUBCARRIER_FREQUENCY_MIN = 1000
@@ -27,14 +31,13 @@ PILOT_SIGNAL_AMPLITUDE = 2
 # パイロット信号の位相
 PILOT_SIGNAL_PHASE = 0
 
-"""
-S/P変換
-データはMSBファーストにする
-配列の要素1つ分は1シンボルに該当する
-"""
-
 
 def serial_to_parallel(x: np.ndarray, bit=8) -> np.ndarray:
+    """
+    S/P変換
+    データはMSBファーストにする
+    配列の要素1つ分は1シンボルに該当する
+    """
     y = np.array([])
     for i in x:
         for j in reversed(range(bit)):
@@ -45,14 +48,12 @@ def serial_to_parallel(x: np.ndarray, bit=8) -> np.ndarray:
     return y
 
 
-"""
-bpsk
-今回は1シンボル1ビット
-ビットが1のところは位相0[rad]に、ビットが0のところは位相π[rad]にする。
-"""
-
-
 def bpsk(x: np.ndarray, bit=8) -> np.ndarray:
+    """
+    bpsk
+    今回は1シンボル1ビット
+    ビットが1のところは位相0[rad]に、ビットが0のところは位相π[rad]にする。
+    """
     y = np.array([])
     for i in x:
         if i == 1:
@@ -86,17 +87,41 @@ def create_spectrum_array(bpsk_phase: np.ndarray) -> np.ndarray:
 
 
 def ifft(X: np.ndarray) -> np.ndarray:
-    x = np.fft.ifft(X)
-    print(x)
-    return x
+    """
+    返り値はt,x
+    """
+
+    x = np.fft.ifft(X, N)
+    return np.arange(N), x
+
+
+def multiply_by_carrier(x: np.ndarray):
+    """
+    返り値はt,x
+    """
+    fc = CARRIER_FREQUENCY
+    t = np.linspace(0, 0.02 - 1 / (2 * fc), int(2 * fc))
+    # IFFTした点の数と1/(2fc)の数が合わないので、線形補間する
+    fitted = interpolate.interp1d(np.linspace(0, 0.02 - 1 / (2 * fc), int(N)), x)
+    fitted_x = fitted(t)
+    # 複素平面上の極座標系から実際の波に変換
+    # そのとき、ωcで変調もする
+    omega_c = 2 * np.pi * fc
+    return t, fitted_x.real * np.cos(omega_c * t) + fitted_x.imag * np.sin(omega_c * t)
 
 
 original_data = np.array([], dtype=np.int64)
 for i in range(12):
-    original_data = np.append(original_data, ord("A") + i)
+    # original_data = np.append(original_data, ord("A") + i)
+    original_data = np.append(original_data, random.randint(0, 255))
 print(original_data)
 sp = serial_to_parallel(original_data)
 bpsk_data = bpsk(sp.astype(np.float64))
 spe = create_spectrum_array(bpsk_data)
-ans = ifft(spe)
-print(ans)
+tk, xk = ifft(spe)
+t, x = multiply_by_carrier(xk)
+fig = plt.figure()
+plt.plot(tk, xk)
+plt.figure()
+plt.plot(t, x)
+plt.show()
