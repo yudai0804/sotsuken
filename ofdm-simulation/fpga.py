8435d0643efababff4615450a9858f3c08e81050
yudai <yudai.yy0804@gmail.com>
2024-12-22 17:04:03 +0900

    fix(Python): output_fft_sramが間違っていたので修正

--- ofdm-simulation/fpga.py
+++ ofdm-simulation/fpga.py
@@ -38,25 +38,31 @@ def output_fft_sram(N: int, _re: NDArray[np.float64], _im: NDArray[np.float64]) 
     re = bit_reverse(_re)
     im = bit_reverse(_im)
     # output
-    for cnt in range(4):
-        s: str = ""
-        for i in range(64):
-            s += f"defparam sp_inst_{cnt:1d}.INIT_RAM_{(i):02X} = 256'h"
-            for j in range(32):
-                index = 32 * i + 31 - j
-                if index >= N:
-                    s += f"00"
-                else:
-                    if cnt == 0:
-                        s += f"{float_to_fixed_q15(im[index]) & 0xff:02X}"
-                    elif cnt == 1:
-                        s += f"{float_to_fixed_q15(im[index]) >> 8:02X}"
-                    elif cnt == 2:
-                        s += f"{float_to_fixed_q15(re[index]) & 0xff:02X}"
+    s: str = ""
+    for fft_cnt in range(2):
+        s += f"// fft{fft_cnt}\n"
+        for sp_cnt in range(4):
+            for i in range(64):
+                s += f"defparam sp_inst_{sp_cnt:1d}.INIT_RAM_{(i):02X} = 256'h"
+                for j in range(32):
+                    index = 32 * i + 31 - j
+                    if fft_cnt == 1:
+                        index += N2
+                    if fft_cnt == 0 and index >= N2:
+                        s += f"00"
+                    elif fft_cnt == 1 and index >= N:
+                        s += f"00"
                     else:
-                        s += f"{float_to_fixed_q15(re[index]) >> 8:02X}"
-            s += ";\n"
-        print(s)
+                        if sp_cnt == 0:
+                            s += f"{float_to_fixed_q15(im[index]) & 0xff:02X}"
+                        elif sp_cnt == 1:
+                            s += f"{float_to_fixed_q15(im[index]) >> 8:02X}"
+                        elif sp_cnt == 2:
+                            s += f"{float_to_fixed_q15(re[index]) & 0xff:02X}"
+                        else:
+                            s += f"{float_to_fixed_q15(re[index]) >> 8:02X}"
+                s += ";\n"
+    print(s)
 
 
 def output_fft1024() -> None:
